5846fcba5c24947515a135f47bd054dd4684f9c0
nga <1801040158@s.hanu.edu.vn>
2020-09-23 09:44:46 +0700

    upload practise

---
+++ practise/practiseWeek2/morse code.py
@@ -0,0 +1,40 @@
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+# Function to encrypt the string
+# according to the morse code chart 
+def convert(message):
+    converted=''
+    for letter in message:
+        if letter!= ' ':
+            converted += MORSE_CODE_DICT[letter.upper()] + ' '
+        else:
+            converted+=' '
+
+    return converted
+def main():
+    string=input("Enter: ")
+    string_converted= convert(string)
+    print(string + " to Morse code: " + string_converted)
+
+
+main()
+
+
+
+
+
+
